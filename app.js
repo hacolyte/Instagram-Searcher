8b8fdc498f50d75ae803b473c760bd539718161a
hacolyte <hacolyte0@gmail.com>
2015-08-28 23:52:07 -0400

    add angular-messages and initial layout/validation

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-angular.module('instagramSearcher', []).
+angular.module('instagramSearcher', ['ngMessages'])
+	.controller('searchController', searchController);
+
+function searchController ($scope){
+	$scope.submit = function(){
+		if( $scope.searchForm.$valid ) {
+    		console.log('The form is valid');
+		} else {
+    		console.log('The form is invalid');
+		}
+	}
+}
